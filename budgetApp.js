f5c332faa366e12605a9a76e08be1b38ad8fa55f
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-12-24 11:59:26 +0000

    Update budgetApp.js
     - added a new way to allow the user a one time extra budget slot should they need it

--- budgetApp.js
+++ budgetApp.js
@@ -9,13 +9,13 @@ const increaseLimitBtn = document.getElementById("increase-limit");
 const limitVal = document.getElementById("limit-value");
 
 
-increaseLimitBtn.addEventListener("click", () => { //Allows the user to increase their monthly budget
+increaseLimitBtn.addEventListener("click", () => { //Allows user to increase their monthly budget allowance
   event.preventDefault();
   limitVal.style.fontSize = "1.3em";
   limitVal.innerText = Number(limitVal.innerText) + 10;
 }); 
 
-decreaseLimitBtn.addEventListener("click", () => { //Allows the user to decreasetheir monthly budget
+decreaseLimitBtn.addEventListener("click", () => { //Allows user to decrease their monthly budget allowance
   event.preventDefault();
   limitVal.style.fontSize = "1.3em";
   limitVal.innerText = Number(limitVal.innerText) - 10;
@@ -26,8 +26,29 @@ decreaseLimitBtn.addEventListener("click", () => { //Allows the user to decrease
 document.getElementById("over-under").textContent = "Your budget position will be displayed here";
 
 
+function addOutgoing() {
+  
+  document.getElementById("add-extra-btn").style.display = "none";
+  let li = document.createElement("li");
+  let input = document.createElement("input");
+  let sym = document.createElement("i");
+      sym.classList.add('far', 'fa-edit');
+      li.setAttribute("id", "extra");
+      input.setAttribute("type", "number");
+      input.classList.add("list-item");
+      input.setAttribute("id", "extra");
+      input.setAttribute("style", "background-color: #FFBDDA;");
+      li.appendChild(sym);
+      li.innerHTML += " Extra Budget Slot:";
+      li.appendChild(input);
+      document.getElementById("outgoings").appendChild(li);
+}
+
+
 function totalBudget() {
   const budgetArr = [...document.getElementsByClassName("list-item")].map((i) => Number(i.value));
+  for (const item of budgetArr) {console.log(item)}
+  
   let totalSum = budgetArr.reduce((acc, el) => acc + el, 0);
   let limitValNum = Number(limitVal.innerText);
   document.getElementById("total-sum").innerText = "£" + totalSum;
@@ -38,11 +59,13 @@ function totalBudget() {
     let selfCare = ((budgetArr[3] / limitValNum) * 100);
     let hobbies = ((budgetArr[4] / limitValNum) * 100);
     let other = ((budgetArr[5] / limitValNum) * 100);
-    let remain = limitValNum - (shopVal + fuelVal + elecGas + selfCare + hobbies + other);
+    let userAdded = ((budgetArr[6] / limitValNum) * 100);
+    let remain = limitValNum - (shopVal + fuelVal + elecGas + selfCare + hobbies + other + userAdded);
 
     let val = `
-      <div id="limit-value" style="background: linear-gradient(to right, var(--green) 0%, var(--green) ${shopVal}%, var(--purple) ${shopVal}%, var(--purple) ${shopVal + fuelVal}%, var(--orange) ${shopVal + fuelVal}%, var(--orange) ${shopVal + fuelVal + elecGas}%, var(--blue) ${shopVal + fuelVal + elecGas}%, var(--blue) ${shopVal + fuelVal + elecGas + selfCare}%, var(--yellow) ${shopVal + fuelVal + elecGas + selfCare}%, var(--yellow) ${shopVal + fuelVal + elecGas + selfCare + hobbies}%, var(--red) ${shopVal + fuelVal + elecGas + selfCare + hobbies}%, var(--red) ${shopVal + fuelVal + elecGas + selfCare + hobbies + other}%, #FFF ${shopVal + fuelVal + elecGas + selfCare + hobbies + other}%)">${limitVal.innerText}</div>
+      <div id="limit-value" style="background: linear-gradient(to right, var(--green) 0%, var(--green) ${shopVal}%, var(--purple) ${shopVal}%, var(--purple) ${shopVal + fuelVal}%, var(--orange) ${shopVal + fuelVal}%, var(--orange) ${shopVal + fuelVal + elecGas}%, var(--blue) ${shopVal + fuelVal + elecGas}%, var(--blue) ${shopVal + fuelVal + elecGas + selfCare}%, var(--yellow) ${shopVal + fuelVal + elecGas + selfCare}%, var(--yellow) ${shopVal + fuelVal + elecGas + selfCare + hobbies}%, var(--red) ${shopVal + fuelVal + elecGas + selfCare + hobbies}%, var(--red) ${shopVal + fuelVal + elecGas + selfCare + hobbies + other}%, var(--pink) ${shopVal + fuelVal + elecGas + selfCare + hobbies + other}%, var(--pink) ${shopVal + fuelVal + elecGas + selfCare + hobbies + other + userAdded}%, #FFF ${shopVal + fuelVal + elecGas + selfCare + hobbies + other + userAdded}%)">${limitVal.innerText}</div>
     `
+    
     limitVal.style.fontSize = "1em";
   if (totalSum < limitVal.innerText) {
     document.getElementById("over-under").textContent = "You are currently under budget!";
